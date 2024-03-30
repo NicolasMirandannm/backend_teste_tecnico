6308b64dae8278463df327c0c39003e40ccf5f13
NicolasMirandannm <nicolasleonardonnm@gmail.com>
2024-03-30 10:34:32 -0400

    spring boot project created

--- src/test/java/backend/BackendApplicationTests.java
+++ src/test/java/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package backend.demo;
+package backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
