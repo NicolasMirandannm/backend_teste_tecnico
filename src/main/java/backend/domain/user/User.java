ad216ee1c6c636771e1ef15d936f0faae27439a1
NicolasMirandannm <nicolasleonardonnm@gmail.com>
2024-03-31 16:03:02 -0400

    created entities

---
+++ src/main/java/backend/domain/user/User.java
@@ -0,0 +1,24 @@
+package backend.domain.user;
+
+import backend.comum.dddSnippets.DomainEntity;
+import backend.comum.valueObjects.UniqueIdentifier;
+import backend.domain.user.entities.Address;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Getter
+public class User extends DomainEntity {
+  
+  private String fullName;
+  private LocalDate birthDate;
+  private List<Address> addresses;
+  private Address mainAddress;
+  
+  public User(UniqueIdentifier id) {
+    super(id);
+  }
+  
+}
---
+++ src/main/java/backend/domain/user/entities/Address.java
@@ -0,0 +1,19 @@
+package backend.domain.user.entities;
+
+import backend.comum.dddSnippets.DomainEntity;
+import backend.comum.valueObjects.UniqueIdentifier;
+import lombok.Getter;
+
+@Getter
+public class Address extends DomainEntity {
+  
+  private String streetAddress;
+  private String postalCode;
+  private String city;
+  private String state;
+  private Integer addressNumber;
+  
+  public Address(UniqueIdentifier id) {
+    super(id);
+  }
+}
