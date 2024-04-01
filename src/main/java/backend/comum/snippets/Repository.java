445770b9e546111940d8b3427092c025892dd64f
NicolasMirandannm <nicolasleonardonnm@gmail.com>
2024-04-01 18:03:17 -0400

    created user repository

--- src/main/java/backend/comum/snippets/Repository.java
+++ src/main/java/backend/comum/snippets/Repository.java
@@ -5,6 +5,5 @@ import backend.comum.valueObjects.UniqueIdentifier;
 public interface Repository<T extends DomainEntity> {
     void save(T entity);
     void delete(T entity);
-    void update(T entity);
     T findById(UniqueIdentifier id);
 }
---
+++ src/main/java/backend/infra/database/repository/UserRepositoryImpl.java
@@ -0,0 +1,47 @@
+package backend.infra.database.repository;
+
+import backend.comum.valueObjects.UniqueIdentifier;
+import backend.domain.aggregate.user.User;
+import backend.domain.repository.UserRepository;
+import backend.infra.database.documents.UserDocument;
+import backend.infra.database.mapper.Mapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
+@Component
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+  
+  private final Mapper<UserDocument, User> mapper;
+  private final MongoTemplate mongoTemplate;
+  
+  @Override
+  public void save(User user) {
+    var userDocument = mapper.toPersistence(user);
+    mongoTemplate.save(userDocument);
+  }
+  
+  @Override
+  public void delete(User user) {
+    var userDocument = mapper.toPersistence(user);
+    mongoTemplate.remove(userDocument);
+  }
+  
+  @Override
+  public User findById(UniqueIdentifier id) {
+    var userDocument = mongoTemplate.findById(id.getValue(), UserDocument.class);
+    return userDocument != null ? mapper.toDomain(userDocument) : null;
+  }
+  
+  @Override
+  public List<User> findByName(String name) {
+    var userDocuments = mongoTemplate.find(query(where("fullName").is(name)), UserDocument.class);
+    return userDocuments.stream().map(mapper::toDomain).toList();
+  }
+}
