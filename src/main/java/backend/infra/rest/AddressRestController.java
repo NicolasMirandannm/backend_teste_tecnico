ae34e29ec1283bf22bb1749bd43326da130eb604
nicolas leonardo <nicolasleonardonnm@hotmail.com>
2024-04-06 20:48:37 -0400

    rest controllers created

--- src/main/java/backend/infra/rest/AddressRestController.java
+++ src/main/java/backend/infra/rest/AddressRestController.java
@@ -1,13 +1,48 @@
 package backend.infra.rest;
 
+import backend.application.addressServices.AddressAbstractService;
+import backend.application.addressServices.dto.AddressDto;
+import backend.application.addressServices.search.AddressSearchService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("address")
 @RequiredArgsConstructor
 public class AddressRestController {
+
+  private final AddressSearchService addressSearchService;
+  private final AddressAbstractService<AddressDto, AddressDto> addressCreationService;
+  private final AddressAbstractService<AddressDto, Void> addressUpdateService;
+  private final AddressAbstractService<String, Void> addressDeleteService;
+
+  @Cacheable(value = "addressesByUser")
+  @GetMapping("/{userId}")
+  public ResponseEntity<List<AddressDto>> findAll(@PathVariable("userId") String userId) {
+    return ResponseEntity.ok(addressSearchService.searchAllBy(userId));
+  }
+
+  @PostMapping("/{userId}")
+  public ResponseEntity<AddressDto> create(@PathVariable("userId") String userId, @RequestBody AddressDto addressDto) {
+    return ResponseEntity.status(201).body(addressCreationService.perform(userId, addressDto));
+  }
+
+  @Async
+  @PutMapping("/{userId}")
+  public void update(@PathVariable("userId") String userId, @RequestBody AddressDto addressDto) {
+    addressUpdateService.perform(userId, addressDto);
+  }
+
+  @Async
+  @DeleteMapping("/{userId}/{addressId}")
+  public void delete(@PathVariable("userId") String userId, @PathVariable("addressId") String addressId) {
+    addressDeleteService.perform(userId, addressId);
+  }
+
 }
--- src/main/java/backend/infra/rest/UserRestController.java
+++ src/main/java/backend/infra/rest/UserRestController.java
@@ -1,13 +1,55 @@
 package backend.infra.rest;
 
+import backend.application.userServices.creation.UserCreationService;
+import backend.application.userServices.delete.UserDeleteService;
+import backend.application.userServices.dto.UserDto;
+import backend.application.userServices.search.UserSearchService;
+import backend.application.userServices.update.UserUpdateService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("user")
 @RequiredArgsConstructor
 public class UserRestController {
+
+  private final UserSearchService userSearchService;
+  private final UserCreationService userCreationService;
+  private final UserUpdateService userUpdateService;
+  private final UserDeleteService userDeleteService;
+
+  @Cacheable(value = "userById")
+  @GetMapping("/{id}")
+  public ResponseEntity<UserDto> findById(@PathVariable(value = "id") String id) {
+    return ResponseEntity.ok(userSearchService.searchById(id));
+  }
+
+  @Cacheable(value = "userByName")
+  @GetMapping
+  public ResponseEntity<List<UserDto>> findByName(@RequestParam("name") String name) {
+    return ResponseEntity.ok(userSearchService.searchByName(name));
+  }
+
+  @PostMapping
+  public ResponseEntity<UserDto> create(@RequestBody UserDto userDto) {
+    return ResponseEntity.status(201).body(userCreationService.perform(userDto));
+  }
+
+  @Async
+  @PutMapping
+  public void update(@RequestBody UserDto userDto) {
+    userUpdateService.perform(userDto);
+  }
+
+  @Async
+  @DeleteMapping("/{id}")
+  public void deleteById(@PathVariable("id") String id) {
+    userDeleteService.perform(id);
+  }
 }
